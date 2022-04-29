dbab4c673a0c84d652c7efcbc88de8f144eac1b8
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-04-30 00:16:22 +0600

    data geted

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import InventoryItems from '../../InventoryItems/InventoryItems';
 import Slider from '../Slider/Slider';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <InventoryItems></InventoryItems>
         </div>
     );
 };
