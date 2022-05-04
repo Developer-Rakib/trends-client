2c8c96af21f53fe22d83aaddef642d35e1bab17d
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-05-04 23:10:04 +0600

    small responsive

--- src/Pages/InventoryItems/InventoryItems.js
+++ src/Pages/InventoryItems/InventoryItems.js
@@ -33,7 +33,6 @@ const InventoryItems = ({ cloth }) => {
                         >Update</button>
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${cloth?.price}</h5>
                     </div>
-
                 </div>
             </div>
         </Fade >
